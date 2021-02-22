f9ee78fea568359ae3d9604affa6744e87d1bdba
kinda-dev <maybe.fabio@gmail.com>
2021-02-22 03:35:39 -0800

    complete frontend auth

---
+++ frontend/src/components/app.js
@@ -0,0 +1,40 @@
+// first basic setup
+
+// import React from 'react';
+// import { AuthRoute, ProtectedRoute } from '../util/route_util';
+// import { Switch } from 'react-router-dom';
+
+// import MainPage from './main/main_page';
+
+// const App = () => (
+//     <Switch>
+//         <AuthRoute exact path="/" component={MainPage} />
+//     </Switch>
+// );
+
+// export default App;
+
+// second setup to implement navbar and session forms
+
+
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+import NavBarContainer from './nav/navbar_container';
+
+import MainPage from './main/main_page';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+
+const App = () => (
+  <div>
+    <NavBarContainer />
+    <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginFormContainer} />
+        <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
+  </div>
+);
+
+export default App;
